3842906a7d4c4512bbfd2a00ea5c7d8cfec42759
Agatha <pedroquadros.chris@gmail.com>
2025-12-28 20:34:29 -0300

    ci: add unit tests

---
+++ src/tests/auth.test.ts
@@ -0,0 +1,29 @@
+import {expect, test} from "vitest";
+import {getAPIKey} from "../api/auth.ts";
+import { IncomingHttpHeaders } from "http";
+
+test('header should have token', () => {
+   const request = {
+    headers: {
+    'authorization': 'ApiKey test-token'
+  } as IncomingHttpHeaders
+};
+  expect(getAPIKey(request.headers)).toBe('test-token')
+})
+
+test('header should bot have token', () => {
+   const request = {
+    headers: {
+    'authorization': 'test-token'
+  } as IncomingHttpHeaders
+};
+  expect(getAPIKey(request.headers)).toBe(null)
+})
+
+test('header doesnt exist', () => {
+   const request = {
+    headers: {
+  } as IncomingHttpHeaders
+};
+  expect(getAPIKey(request.headers)).toBe(null)
+})
